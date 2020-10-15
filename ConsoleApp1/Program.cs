14e6fd75b506c1ed4ae3f77dbfee6338a1cd5607
Synx-x <siyandamtamo@gmail.com>
2020-10-16 00:04:42 +0200

    Finished Calculator App

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,9 +8,44 @@ namespace ConsoleApp1
 {
     class Program
     {
+       
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            double result = 0.0;
+
+            Console.WriteLine("Welcome to the Calculator App!");
+            Console.Write("Enter Your First Number: ");
+            double a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter which operator you want to use: ");
+            string op = Console.ReadLine();
+
+            Console.WriteLine("Enter your Last Number: ");
+            double b = double.Parse(Console.ReadLine());
+
+
+            switch (op)
+            {
+                case "+":
+                    result = a + b;
+                    break;
+                case "-":
+                    result = a - b;
+                    break;
+                case "*":
+                    result = a * b;
+                    break;
+                case "/":
+                    result =a / b;
+                    break;
+                default:
+                    Console.WriteLine("INCORRECT INPUT!");
+                    break;
+            }
+            
+            Console.WriteLine("Your Answer is: "+result);
+            Console.ReadLine();
+
         }
     }
 }
